c20bfad47bfe25f4a4ab886c3cb7d943450796de
Okolokulak <71151545+Okolokulak@users.noreply.github.com>
2022-05-02 17:02:31 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,60 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  options.separator === undefined
+    ? options.separator = "+"
+    : options.separator = String(options.separator);
+  options.additionSeparator === undefined
+    ? options.additionSeparator = "|"
+    : options.additionSeparator = String(options.additionSeparator);
+  //даём дефолтные значения, указанные в задаче и удостоверяемся, что пришедшее станет строкой
+
+  function repeat(str, separator, repeatTimes) {
+    if (str === undefined) str = "";
+    if (repeatTimes === undefined) repeatTimes = 1; // опять задаём дефолт
+    let arr = new Array;
+
+    for (let i = 0; i < repeatTimes; i++) {
+      arr.push(String(str));
+    }
+    let result = arr.join(separator);
+
+    return result;
+  }
+
+  let addition = repeat(
+    options.addition,
+    options.additionSeparator,
+    options.additionRepeatTimes
+  ); //сначала генерируем допольнительное, чтобы потом использовать в генерации основы
+
+
+  let main = repeat(
+    str,
+    addition + options.separator,
+    options.repeatTimes
+  ); //используем доп. для генерации основной строки
+  
+  let full = main + addition; 
+  
+  return full;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
